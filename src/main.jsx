401b04e4b985d6e30602d6cf1461ebdd3e577178
Heitor Denech <heitor.denech@gmail.com>
2025-06-13 15:14:48 -0300

    segundo commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './global.css'   // Importa o CSS global aqui
+import '../src/styles/Global.css'  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
