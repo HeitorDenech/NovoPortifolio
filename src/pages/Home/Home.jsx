60e75c7f4673387fddcbd382c11f02c3f861f763
Heitor Denech <heitor.denech@gmail.com>
2025-06-20 00:34:03 -0300

    corrindo bug css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,13 +8,13 @@ function Home() {
         <section className='home'>
 
             <div className='container-introducao'>
-                <div className='container-interno-introducao'>
-                    <h1 data-aos="fade-right">Web designer</h1>
-                    <p data-aos="fade-right">Olá! me chamo <a href="https://www.youtube.com/watch?v=EOA1wBw_Jt4" target='_blank'>H</a>eitor Denech, sou apaixonado por transformar ideias em experiências visuais criativas e funcionais. Através do design responsivo,
+                <div className='container-interno-introducao' data-aos="fade-right">
+                    <h1>Web designer</h1>
+                    <p>Olá! me chamo <a href="https://www.youtube.com/watch?v=EOA1wBw_Jt4" target='_blank'>H</a>eitor Denech, sou apaixonado por transformar ideias em experiências visuais criativas e funcionais. Através do design responsivo,
                         interfaces intuitivas e foco na usabilidade, busco criar sites que não apenas sejam bonitos,
                         mas que realmente funcionem para o usuário.</p>
-                    <p data-aos="fade-right">Neste portfólio, você encontrará projetos que refletem meu aprendizado contínuo, atenção aos detalhes e compromisso com a inovação. </p>
-                    <p data-aos="fade-right">Seja bem-vindo(a) ao meu universo digital!</p>
+                    <p>Neste portfólio, você encontrará projetos que refletem meu aprendizado contínuo, atenção aos detalhes e compromisso com a inovação. </p>
+                    <p>Seja bem-vindo(a) ao meu universo digital!</p>
 
                     <div className='container-interno-introducao-btn'>
 
