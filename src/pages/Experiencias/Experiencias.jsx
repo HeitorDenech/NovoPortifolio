4d9eb215232236d343be0b8ceb7456c2dc0df4b6
HeitorDenech <heitor.denech@gmail.com>
2025-08-27 22:18:02 -0300

    fazendo pagina de experiencias mobile

--- src/pages/Experiencias/Experiencias.jsx
+++ src/pages/Experiencias/Experiencias.jsx
@@ -2,26 +2,40 @@ import './Experiencias.css';
 import campus from "../../assets/campus-Sao-Leopoldo.jpg";
 import pucrs from "../../assets/pucrs.jpg";
 
+
 function Experiencias() {
     return (
         <section className='experiencias'>
+
+            {/* --------PARTE-MOBILE---------- */}
+
+            <section className='container-mobile-experiencias'>
+
+                <h1 data-aos="fade-right">Senac-rs (2022 - 2024)</h1>
+                <p>Html</p>
+                <p>Css</p>
+                <p>JavaScript</p>
+                <p>Mysql2</p>
+
+            </section>
+
             <section className='container-esquerda-experiencias'>
 
-                <div className='container-esquerda-1' data-aos="fade-right">
+                <div className='container-esquerda-1' data-aos="fade-up">
 
-                    <h1>Senac</h1>
+                    <h1>Senac-rs</h1>
 
                     <p>Minha primeira vez vendo a programação, foi no meu primeiro ano do ensino médio, em 2022, no
-                        <a href="https://www.senacrs.com.br/unidade/39" target='blank'> Senac RS </a>
+                        <a href="https://www.senacrs.com.br/unidade/39" target='blank'> Senac-RS </a>
                         localizado em São Leopoldo. Por ser um curso técnico, tive contato com várias
                         linguagens, como python, html, css e javascript. Ao decorrer do curso, comecei a me
                         interessar muito pelo front-end, fazeno figmas, me aprofundando mais em
-                        html, css e javascript e em react.
+                        html, css e javascript.
                     </p>
 
                 </div>
 
-                <div className='container-esquerda-2' data-aos="fade-right">
+                <div className='container-esquerda-2' data-aos="fade-up">
                     <img src={pucrs} alt="" />
                 </div>
 
@@ -33,11 +47,11 @@ function Experiencias() {
 
             <section className='container-direita-experiencias'>
 
-                <div className='container-direita-1' data-aos="fade-left">
+                <div className='container-direita-1' data-aos="fade-up">
                     <img src={campus} alt="" />
                 </div>
 
-                <div className='container-direita-2' data-aos="fade-left">
+                <div className='container-direita-2' data-aos="fade-up">
                     <h1>PUC-RS</h1>
 
                     <p>Atualmente estou cursando Análise e Desenvolvimento de Sistemas na
@@ -45,7 +59,7 @@ function Experiencias() {
                         , onde estou me aprofundando mais em back-end, estudando Java e banco de dados.
                         Também estou aprendendo mais sobre estrutura de dados e
                         algoritmos, o que está me ajudando muito a pensar como um desenvolvedor.
-                        Além disso, participei da <a href="https://www.ages.pucrs.br/" target='blank'>AGES</a>, um grupo de estudos 
+                        Além disso, participei da <a href="https://www.ages.pucrs.br/" target='blank'>AGES</a>, um grupo de estudos
                         onde tive a oportunidade de aprender mais sobre front-end e back-end.
                     </p>
                 </div>
