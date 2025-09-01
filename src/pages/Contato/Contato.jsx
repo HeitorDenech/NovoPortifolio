c8fbde38d408d175b7dee78fbc01760cf983dcb3
HeitorDenech <heitor.denech@gmail.com>
2025-09-01 19:30:13 -0300

    front da pagina de contato feita

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,6 +1,27 @@
+import React, { useState } from 'react';
 import './Contato.css';
 
 function Contato() {
+    const [formData, setFormData] = useState({
+        nome: '',
+        email: '',
+        mensagem: ''
+    });
+
+    // Atualiza os valores do formulário
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    // Envia os dados (vamos conectar ao back depois)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(formData); // temporário, só pra testar
+    };
+
     return (
         <section className='contato'>
             <h1>
@@ -12,18 +33,24 @@ function Contato() {
                 {/* <h1>Envie-me uma mensagem!</h1> */}
 
                 <div className='contato-container-nome'>
-                    <p>nome *</p>
+                    <p>nome * <i class="bi bi-person-fill"></i></p>
                     <input type="text" placeholder='Seu Nome' />
                 </div>
 
                 <div className='contato-container-email'>
-                    <p>endereço de e-mail *</p>
+                    <p>endereço de e-mail * <i class="bi bi-envelope-fill"></i></p>
                     <input type="email" placeholder='Seu endereço de E-mail' />
                 </div>
 
                 <div className='contato-container-mensagem'>
-                    <p>mensagem *</p>
+                    <p>mensagem * <i class="bi bi-pencil-fill"></i></p>
                     <textarea placeholder='Escreva sua mensagem aqui' rows="5"></textarea>
+                </div>
+
+                <div className='contato-container-botao'>
+                    <button>
+                        Enviar <i class="bi bi-send-fill"></i>
+                    </button>
                 </div>
 
             </section>
